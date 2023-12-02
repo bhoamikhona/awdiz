8e10ed4e77fba67bf29b4c7f279bb5a1bd1a7053
Bhoami K Khona <bhoamikhona@gmail.com>
2023-12-02 20:09:26 +0530

    day 38

---
+++ day-38/README.md
@@ -0,0 +1,6 @@
+ctrl + spacebar => code suggestions
+
+- Creating default path for page not found
+- Navbar component
+- footer component
+- useNavigate hook
---
+++ day-38/frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import NoPageFound from "./components/NoPageFound";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="*" element={<NoPageFound />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
