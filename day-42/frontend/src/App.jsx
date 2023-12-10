4a132343322e51912671de09810260c14885fba1
Bhoami K Khona <bhoamikhona@gmail.com>
2023-12-10 12:56:28 +0530

    day 42

--- day-42/frontend/src/App.jsx
+++ day-42/frontend/src/App.jsx
@@ -15,8 +15,40 @@ import UseEffect3 from "./components/08-12/UseEffect3";
 import UseEffect4 from "./components/08-12/UseEffect4";
 import UseParams from "./components/08-12/UseParams";
 import DynamicRouting from "./components/08-12/DynamicRouting";
+import PropsMaps from "./components/09-12/PropsMaps";
+import MapObject from "./components/09-12/MapObject";
+import StyledComponent from "./components/09-12/StyledComponent";
+import { useState } from "react";
 
 function App() {
+  const [myName, setMyName] = useState("Bhoami");
+  const fruit1 = {
+    id: 1,
+    img: "https://i.pinimg.com/564x/d1/7a/77/d17a77389b34daabcfdd58d78fce5c5d.jpg",
+    name: "Strawberry",
+    price: "₹420 per kg",
+  };
+
+  const fruit2 = {
+    id: 2,
+    img: "https://i.pinimg.com/564x/cc/f2/a6/ccf2a62d89c0bc33880e119c9f576a17.jpg",
+    name: "Cherry",
+    price: "₹600 per kg",
+  };
+
+  const fruit3 = {
+    id: 3,
+    img: "https://i.pinimg.com/564x/d4/4b/d9/d44bd99516b8557ee5551818d9694765.jpg",
+    name: "Orange",
+    price: "₹70 per kg",
+  };
+
+  const [myFruits, setFruits] = useState([fruit1, fruit2, fruit3]);
+
+  function ChangeName() {
+    setMyName("Bhoami K Khona");
+  }
+
   return (
     <div className="App">
       <Navbar />
@@ -33,6 +65,22 @@ function App() {
         <Route path="/use-effect-4" element={<UseEffect4 />} />
         <Route path="/dynamic-routing" element={<DynamicRouting />} />
         <Route path="/use-params/:age" element={<UseParams />} />
+        <Route
+          path="/props-map"
+          element={
+            <PropsMaps
+              myName={myName}
+              myAge="25"
+              ChangeName={ChangeName}
+              myFruits={["strawberry", "cherry", "orange"]}
+            />
+          }
+        />
+        <Route
+          path="/map-object"
+          element={<MapObject myFruitsData={myFruits} />}
+        />
+        <Route path="/styled-component" element={<StyledComponent />} />
       </Routes>
       <Footer />
     </div>
