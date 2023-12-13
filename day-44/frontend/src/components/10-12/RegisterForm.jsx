8bf5c720c32fcc00bafb180b37df5593870cb809
Bhoami K Khona <bhoamikhona@gmail.com>
2023-12-13 11:53:27 +0530

    day 44

---
+++ day-44/frontend/src/components/10-12/RegisterForm.jsx
@@ -0,0 +1,110 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const RegisterForm = () => {
+  const router = useNavigate();
+
+  const [userData, setUserData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  console.log(userData, "userData");
+  async function handleSubmit(e) {
+    e.preventDefault();
+    if (
+      userData.name &&
+      userData.email &&
+      userData.password &&
+      userData.confirmPassword
+    ) {
+      if (userData.password === userData.confirmPassword) {
+        try {
+          // const response = await axios.post('http://localhost:8000/register', { userData })
+
+          const response = {
+            data: { success: true, messsage: "Registeration Complete." },
+          };
+
+          if (response.data.success === true) {
+            alert(response.data.messsage);
+            setUserData({
+              name: "",
+              email: "",
+              password: "",
+              confirmPassword: "",
+            });
+            router("/login");
+          }
+        } catch (error) {
+          console.log(error);
+          alert(error.response.data.messsage);
+        }
+      } else {
+        alert("Password and Confirm Password not matched.");
+      }
+    } else {
+      alert("All fields are required.");
+    }
+  }
+
+  function handleChange(e) {
+    // console.log(e.target.value, e.target.name)
+    setUserData({ ...userData, [e.target.name]: e.target.value });
+  }
+  return (
+    <div>
+      <h1>Register</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Name :</label>
+        <br />
+        <input
+          autoComplete="off"
+          type="text"
+          required
+          onChange={handleChange}
+          name="name"
+          value={userData.name}
+        />
+        <br />
+        <label>Email :</label>
+        <br />
+        <input
+          autoComplete="off"
+          type="email"
+          required
+          onChange={handleChange}
+          name="email"
+          value={userData.email}
+        />
+        <br />
+        <label>Password</label>
+        <br />
+        <input
+          autoComplete="off"
+          type="password"
+          required
+          onChange={handleChange}
+          name="password"
+          value={userData.password}
+        />
+        <br />
+        <label>Confirm Password</label>
+        <br />
+        <input
+          autoComplete="off"
+          type="password"
+          required
+          onChange={handleChange}
+          name="confirmPassword"
+          value={userData.confirmPassword}
+        />
+        <br />
+        <input type="submit" value="Register" />
+      </form>
+    </div>
+  );
+};
+
+export default RegisterForm;
