a25bf76339aabce7fa2dd134ad081135a0d92bf4
Bhoami K Khona <bhoamikhona@gmail.com>
2024-01-09 20:36:22 +0530

    day 57

---
+++ day-57/README.md
@@ -0,0 +1,6 @@
+# Setup Steps
+
+- 01: package.json - `npm init -y`
+- 02: Create index.js file
+- 03: install nodemon - `npm i nodemon`
+- 04: Add start script in package.json file and add value - `nodemon ./index.js`
---
+++ day-57/index.js
@@ -0,0 +1,17 @@
+// Express
+import fs from "fs";
+
+// fs.readFile("test.txt", "utf-8", (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
+
+const content = "This is the content to be written in the file.";
+// Create a new fule and write content in it.
+fs.writeFile("example.txt", content, (err) => {
+  if (err) {
+    console.log(`Error writing this file: ${err}`);
+  } else {
+    console.log(`File created and content written successfully!`);
+  }
+});
