a4f6686961c3713f82f9bc23b9df6855c986cbce
Bhoami K Khona <bhoamikhona@gmail.com>
2024-03-31 08:50:25 +0530

    30 March - Creating a search bar for all fake store api

--- client/src/components/10-03/FakeStoreAllProducts.jsx
+++ client/src/components/10-03/FakeStoreAllProducts.jsx
@@ -5,7 +5,10 @@ import { useNavigate } from "react-router-dom";
 
 export default function FakeStoreAllProducts() {
   const [allProducts, setAllProducts] = useState([]);
-  console.log(allProducts);
+  // console.log(allProducts);
+  const [filterProducts, setFilterProducts] = useState([]);
+
+  const [search, setSearch] = useState("");
 
   const router = useNavigate();
 
@@ -14,6 +17,7 @@ export default function FakeStoreAllProducts() {
       const response = await axios.get("https://fakestoreapi.com/products");
       if (response?.data.length) {
         setAllProducts(response.data);
+        setFilterProducts(response.data);
       }
     } catch (error) {
       console.log(error);
@@ -25,15 +29,40 @@ export default function FakeStoreAllProducts() {
     router(`/fake-single-product/${id}`);
   }
 
+  const handleChange = function (e) {
+    console.log(e.target.value);
+    setSearch(e.target.value);
+
+    let userword = e.target.value.toLowerCase();
+
+    const filteredProducts = allProducts.filter((product) => {
+      return product.title.toLowerCase().includes(userword);
+    });
+
+    console.log("filteredProducts", filteredProducts);
+    setFilterProducts(filteredProducts);
+  };
+
   useEffect(() => {
     getProducts();
   }, []);
   return (
     <div>
       <h1>Fake Store All Products</h1>
-      {allProducts?.length ? (
+
+      <div>
+        <label htmlFor="search">Search Product:</label>
+        <input
+          value={search}
+          type="text"
+          placeholder="Men Cotton Jacket"
+          onChange={handleChange}
+        />
+      </div>
+
+      {filterProducts?.length ? (
         <div className="products-container">
-          {allProducts.map(({ title, image, id }) => (
+          {filterProducts.map(({ title, image, id }) => (
             <div onClick={() => redirect(id)} className="product-card">
               <div className="product-img-container">
                 <img src={image} alt={title} className="product-img" />
