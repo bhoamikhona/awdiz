2e3bec6d9414b8474b17f8cbd7be72b4e40f5495
Bhoami K Khona <bhoamikhona@gmail.com>
2024-04-06 12:45:05 +0530

    6th April 2024

---
+++ server/06-04/StringUtils.js
@@ -0,0 +1,5 @@
+const capitalString = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+
+const reverseString = (str) => str.split("").reverse().join("");
+
+module.exports = { capitalString, reverseString };
---
+++ server/06-04/anotherfile.js
@@ -0,0 +1,4 @@
+const myModule = require("./StringUtils.js");
+
+console.log(myModule.reverseString("welcome"));
+console.log(myModule.capitalString("welcome"));
